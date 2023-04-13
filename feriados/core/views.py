432516b87cd9052f01a7be973af029ab4c5a1a15
Klayvert <klayvert.ryan16@gmail.com>
2023-04-13 00:14:55 -0300

    Ajustando views

--- feriados/core/views.py
+++ feriados/core/views.py
@@ -4,6 +4,7 @@ import datetime
 
 def verificar_feriado(request):
     data_atual = datetime.date.today()
+    #data_atual = datetime.date(day=25, month=12, year=2022)
     try:
         feriados = Feriados.objects.get(dia=data_atual.day, mes=data_atual.month)
         return render(request, 'verificar_feriado.html', {'feriados': feriados})
